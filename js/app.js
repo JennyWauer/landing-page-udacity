e2c486f114ad634fd5d3b6eb0329e6594e7eacb3
JennyWauer <jkfoil1@gmail.com>
2020-05-14 15:27:16 -0500

    feat: added h2 titles to section elements

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function checkSections() {
+    let sections = document.getElementsByClassName('section');
+    let sectionArray = [];
+    for (var section of sections) {
+        let section = ""
+        section = sections[index].id;
+        sectionArray.push(section);
+    }
+}
