c535cccd0b939635d5f4bdd00c1dc9944d49e09c
JennyWauer <jkfoil1@gmail.com>
2020-05-14 19:55:24 -0500

    docs: updated element classes

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function checkSections() {
-    let sections = document.getElementsByClassName('section');
-    let sectionArray = [];
-    for (var section of sections) {
-        let section = ""
-        section = sections[index].id;
-        sectionArray.push(section);
-    }
-}
