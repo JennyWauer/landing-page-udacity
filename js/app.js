aba67044af3137143ac0f881022e6fba33cd2dc2
JennyWauer <jkfoil1@gmail.com>
2020-05-15 08:27:45 -0500

    fix: updating nav bar loop

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@ function populateNavList() {
     /*array.from property learned from: https://www.gavsblog.com/blog/htmlcollection-foreach-loop-convert-object-to-array-javascript */
     let sections = Array.from(document.getElementsByClassName('section'));
     for (var i of sections) {
-        var navItem = document.createElement("li");
-        navItem.innerHTML = sections[i].textContent;
-        navItem.appendChild(navItem);
+        let content = document.getElementsByTagName('h2');
+        let navItem = document.createElement("li");
+        for (var y of content) {
+        navItem.innerHTML = content.textContent;
+        navBar.appendChild(navItem);
+        }
                
     }
 }
