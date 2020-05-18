a82914ed7f3c12f145d3cf79c153eba39a38ee89
JennyWauer <jkfoil1@gmail.com>
2020-05-18 11:15:39 -0500

    doc: adding comments throughout project

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+/*Function to populate Navigation Bar */
 function populateNavList() {
     const navBar = document.querySelector('.navBar')
     /*array.from property learned from: https://www.gavsblog.com/blog/htmlcollection-foreach-loop-convert-object-to-array-javascript */
+    /*function assisted by html documentation at: https://www.w3schools.com/jsref/met_document_createelement.asp */
     const sections = Array.from(document.querySelectorAll('.section'));
     sections.forEach(function (item) {
         const navItem = document.createElement('li');
